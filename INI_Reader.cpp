9f2c84dd9d9ddb38b3328b23745e3a8b4035e0f5
IlyaMikhasev <113455026+IlyaMikhasev@users.noreply.github.com>
2023-02-24 20:13:40 +0500

    Update INI_Reader.cpp

--- INI_Reader.cpp
+++ INI_Reader.cpp
@@ -96,21 +96,21 @@ void INI_Reader::Replacement(const std::string& head, const std::string& key, co
 		for (auto it = _section[head].begin(); it != _section[head].end(); it ++) {
 			if (it->first == key) {
 				it->second = var;
-				std::cout << "Çíà÷åíèå èçìåíåíî\n";
+				std::cout << "Ã‡Ã­Ã Ã·Ã¥Ã­Ã¨Ã¥ Ã¨Ã§Ã¬Ã¥Ã­Ã¥Ã­Ã®\n";
 			}
 		}
 	}
 	else {
 		std::pair<std::string, std::string> keyVar = { key,var };
 		_section[head].push_back(keyVar);
-		std::cout << "Ñîçäàí íîâûé ğàçäåë ñî çíà÷åíèåì\n";
+		std::cout << "Ã‘Ã®Ã§Ã¤Ã Ã­ Ã­Ã®Ã¢Ã»Ã© Ã°Ã Ã§Ã¤Ã¥Ã« Ã±Ã® Ã§Ã­Ã Ã·Ã¥Ã­Ã¨Ã¥Ã¬\n";
 	}
 }
 
 const std::string INI_Reader::GetFile() const{
 	std::string file;
 	for ( auto [head, value] : _section) {
-		file += (head+'\n');
+		file += ("["+head+"]\n");
 		for ( auto it = value.begin(); it != value.end(); it++) {
 			file += (it->first + '=' + it->second + '\n');
 		}
